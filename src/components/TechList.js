cf1f2462bbc543b60e4023aeea10d129416f89d2
Charles Pereira <charlesti.ptu@gmail.com>
2019-10-30 00:23:10 -0300

    Ciclo de vida do Componente: DidMount, DidUpdate e WillMount

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -5,12 +5,29 @@ import TechItem from './TechItem';
 class TechList extends Component {
   state = {
     newTech: '',
-    techs: [
-      'Node JS',
-      'React JS',
-      'React Native1',
-    ]
+    techs: []
   };
+
+  // Executado assim que o componente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  componentDidUpdate(prevProps, PrevState) {
+    if (PrevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillMount() {
+
+  }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
