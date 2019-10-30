c7aaef5eab735cc1fee5edd1df97d0491bcd542b
Charles Pereira <charlesti.ptu@gmail.com>
2019-10-29 23:00:39 -0300

    adicinando componentes de importação de imagem

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+import profile from './assets/Charles.jpg';
+
+function App() {
+  return <img src={profile} width="200" />
+
+}
+
+export default App;
